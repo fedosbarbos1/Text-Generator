791d849b5fb7f6f2a9d281daaf5c12c9a521210d
Mariia Fedosova <93509495+fedosbarbos1@users.noreply.github.com>
2022-07-14 17:15:19 +0300

    Stage 6
    Extending the program to create a Markov model based on trigrams in order to generate more sensible sentences.

---
+++ Stage 6.py
@@ -0,0 +1,76 @@
+import json
+import requests
+import pandas as pd
+import re
+import numpy as np
+
+url = "https://randomuser.me/api/?results=1000"
+my_params = {"inc": ["gender, email, phone"]}
+response1 = requests.get(url, params=my_params)
+user_data1 = response1.text
+
+saved_data1 = json.loads(user_data1)
+data1 = saved_data1["results"]
+# print(data1)
+
+DF = pd.DataFrame(data1)
+# print(DF)
+
+DF["name"] = DF["email"].apply(lambda name: name.capitalize())
+
+def includes_words(name):
+    inc_name = name.find("@")
+    if inc_name == -1:
+        return inc_name
+    else:
+        return name[:inc_name + 0]
+
+def get_name(name):
+    name = includes_words(name)
+    name_list = name.split(".")
+    name = name_list[0]
+    return name
+
+def get_surname(name):
+    name = includes_words(name)
+    name_list = name.split(".")
+    surname = name_list[1]
+    return surname.capitalize()
+
+DF["name"] = DF["name"].apply(get_name)
+DF["surname"] = DF["surname"].apply(get_surname)
+# print(DF)
+
+# DF_gender = DF[DF["gender"] == "female"]
+# DF_gender.to_csv("female.csv", sep=",", index=False, encoding="utf-8")
+
+regex_structure1 = []
+regex_structure2 = []
+regex_structure3 = []
+regex_structure4 = []
+other_numbers = []
+
+for n in DF["phone"]:
+    reg1 = re.findall(r'\d{2}-\d{3}-\d{3}', n)
+    reg2 = re.findall(r'[(]\d{3}[)]-\d{3}-\d{4}', n)
+    reg3 = re.fullmatch(r'\d*-\d+|\d+-\d*', n)
+    reg4 = re.findall(r'\d+ +', n)
+    if reg1 != []:
+        regex_structure1.append(n)
+    elif reg2 != []:
+        regex_structure2.append(n)
+    elif reg3 != None:
+        regex_structure3.append(n)
+    elif reg4 != []:
+        regex_structure4.append(n)
+    else:
+        other_numbers.append(n)
+
+print(regex_structure1)
+print(regex_structure2)
+print(regex_structure3)
+print(regex_structure4)
+print(other_numbers)
+
+
+
